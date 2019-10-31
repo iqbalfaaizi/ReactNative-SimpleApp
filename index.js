76b14eb45a9cb2f1ce1df350894ab1ae0aac7875
iqbalalfaaizi <iqbalalfaaizi@outlook.com>
2019-10-31 10:41:01 +0700

    nope

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 /**
  * @format
  */
-import React, {Component} from 'react'
 import {AppRegistry} from 'react-native';
-// import App from './App';
-import App from './src/App';
+import App from './App';
 import {name as appName} from './app.json';
 
-export default class ReduxCounter extends Component {
-    render() {
-        return(
-            <App />
-        )
-    }
-}
-
-// AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent('ReduxCounter', () => ReduxCounter )
+AppRegistry.registerComponent(appName, () => App);
